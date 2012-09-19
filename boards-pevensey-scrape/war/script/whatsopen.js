a7082445b31d08a3c1f74b8c1bf63da8026bf3df
henryw <henryw374@gmail.com>
2012-09-19 19:12:58 +0100

    some basic display of opeining if not open now

--- boards-pevensey-scrape/war/script/whatsopen.js
+++ boards-pevensey-scrape/war/script/whatsopen.js
@@ -4,6 +4,7 @@ var initialize = function() {
 
     // bind to the marker detail tap event
     //  $('a[href="#marker-detail"]').live(supportsTouch ? 'tap' : 'click', some fn);
+    if(window.location.hash !== ""){ window.location.hash = ""}
 
     if (navigator.geolocation)
     {
@@ -89,20 +90,58 @@ var processServiceResults = function(results, status){
             if(aOpen){return -1;}
             else{return 1;}
         });
-        var listElement = "<ul data-role='listview' data-inset='true' data-theme='d'>";
+        var listElement = ["<ul data-role='listview' data-inset='true' data-theme='d'>"];
         for (var i = 0; i < results.length; i++) {
             var place = results[i];
             var marker = createMarker(place);
 
-            isOpen = place.opening_hours && place.opening_hours.open_now ? "yes" : "no";
-            listElement += "<li><a href='#'><h1>"+place.name+"</h1><p>Distance: "+1+"km</p> <p>Open: "+isOpen+"</p></a></li>";
+            isOpen = place.opening_hours && place.opening_hours.open_now;
+
+            listElement.push("<li id='list_"+place.reference +"'><h1>"+place.name+"</h1><p>Distance: "+1+"km</p>");
+            if(isOpen){
+                listElement.push(" <p>Open: Yes</p>");
+            }
+            else{
+                listElement.push(" <p>Open: No</p><a href='#' data-role='button' onclick='lookupOpening(event,\""+place.reference+"\")'>When does it Open?</a>");
+            }
+
+            listElement.push("</li>");
+
         }
 
-        var previouslyRendered = $('#listHolder').children().length > 0;
-           
-        $('#listHolder').html(listElement+"</ul>");
+        var previouslyRendered = $('#listHolder').attr('class');
+
+        $('#listHolder').html(listElement.join('')+"</ul>");
         previouslyRendered && $('#list_results').trigger("create");
     }
+}
+
+var lookupOpening = function(event,placeRef){
+    event.preventDefault();
+    placesService.getDetails({reference:placeRef},detailsResponse);
+}
+
+var detailsResponse = function(place,status){
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        var periods = valOrNull(place,"opening_hours.periods");
+        if(!periods){ alert("unfortunately this place has not entered hours of opening"); return;}
+        var date = new Date();
+        var todaysOpeningTimes = periods[date.getDay()];
+        var nowHoursAndMinutesAsNumber = Number(""+date.getHours()+date.getMinutes());
+var todayOpeningTime = Number(todaysOpeningTimes.open.time);
+//alert(todayOpeningTime+" "+nowHoursAndMinutesAsNumber)
+        if(todayOpeningTime > nowHoursAndMinutesAsNumber){
+             alert("opening at "+todaysOpeningTimes.open.time);
+             return;
+        }
+        var tomorrowsOpeningTimes = periods[date.getDay()+1];
+        alert("unlucky, it closed at "+todaysOpeningTimes.close.time+" but opens again at "+tomorrowsOpeningTimes.open.time);
+    }
+    else
+    {
+        alert("unable to load opening times");
+    }
+
 }
 
 var createMarker = function (place) {
@@ -144,14 +183,90 @@ var myDoAjax = function(url, callback){
 var google = {maps: {places:{PlacesServiceStatus:{OK :'OK'}} }}
 
 var generateMapAndListResults = function(positionConfig){
-    processServiceResults([{opening_hours:{open_now:false},name:'pemberly'},
-                           {opening_hours:{open_now:true},name:'gosford'}],'OK');
+    processServiceResults([{reference:0,opening_hours:{open_now:false},name:'pemberly'},
+                           {reference:1,opening_hours:{open_now:true},name:'gosford'}],'OK');
+
+    placesService = {getDetails:function(){
+        detailsResponse({opening_hours: {periods :[
+            {
+               "close" : {
+                  "day" : 0,
+                  "time" : "1400"
+               },
+               "open" : {
+                  "day" : 0,
+                  "time" : "0900"
+               }
+            },
+            {
+               "close" : {
+                  "day" : 1,
+                  "time" : "1800"
+               },
+               "open" : {
+                  "day" : 1,
+                  "time" : "0800"
+               }
+            },
+            {
+               "close" : {
+                  "day" : 2,
+                  "time" : "1800"
+               },
+               "open" : {
+                  "day" : 2,
+                  "time" : "0800"
+               }
+            },
+            {
+               "close" : {
+                  "day" : 3,
+                  "time" : "1800"
+               },
+               "open" : {
+                  "day" : 3,
+                  "time" : "0800"
+               }
+            },
+            {
+               "close" : {
+                  "day" : 4,
+                  "time" : "1800"
+               },
+               "open" : {
+                  "day" : 4,
+                  "time" : "0800"
+               }
+            },
+            {
+               "close" : {
+                  "day" : 5,
+                  "time" : "1800"
+               },
+               "open" : {
+                  "day" : 5,
+                  "time" : "0800"
+               }
+            },
+            {
+               "close" : {
+                  "day" : 6,
+                  "time" : "1400"
+               },
+               "open" : {
+                  "day" : 6,
+                  "time" : "0900"
+               }
+            }
+         ]}},'OK');
+    }};
 
 }
 
 var createMarker = function(){}
 
 var initialize = function(){
+    if(window.location.hash !== ""){ window.location.hash = ""}
     browserPosition({latitude:50,longitude:0});
 }
 
